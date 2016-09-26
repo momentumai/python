82a42fe5a601b8da078a04a4b56efb3dbf4b1e6d
Zoltan Molnar <zoltan.molnar@brandvee.com>
2016-09-26 14:17:39 +0100

    added experiments to campaigns

--- application/views/history/bigquery/campaign-query.sql
+++ application/views/history/bigquery/campaign-query.sql
@@ -1,12 +1,17 @@
 SELECT
   team_id,
-  campaign,
+  IF(campaign > 0, STRING(campaign), experiment) as campaign,
   SUM(1) AS share
 FROM [{{dataset}}.views_{{table}}]
 WHERE
-  is_share
-  AND campaign > 0 AND
-  time = SEC_TO_TIMESTAMP({{time}})
+  is_share AND
+  time = SEC_TO_TIMESTAMP({{time}}) AND (
+    campaign > 0 OR (
+      experiment IS NOT NULL AND
+      experiment != '' AND
+      experiment != '0'
+    )
+  )
 GROUP BY
   team_id,
   campaign
